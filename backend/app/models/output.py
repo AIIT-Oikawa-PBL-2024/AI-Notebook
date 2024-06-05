34d846706cfdbf63388c9bfd8acc28d3ab9cd493
ysugimoto <ysugimoto@example.com>
2024-06-05 22:01:31 +0900

    Add output model to create output table in db

---
+++ backend/app/models/output.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from app.database import Base
+
+
+# filesテーブルを定義
+class File(Base):
+    __tablename__ = "output"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    user_id = Column(
+        Integer, ForeignKey("users.id"), nullable=False
+    )  # 外部キー制約を追加
+    # Varcharの最大長にするためLengthは未指定
+    output = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+
+    user = relationship("User", back_populates="files")
