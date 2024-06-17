cbc29b9312ce7d4622950b9a2769bb9ad9fc4e72
sayzkkgw <sayzkkgw@gmail.com>
2024-06-18 00:40:08 +0900

    fix: to check fixed prod code

--- frontend/tests/test_output.py
+++ frontend/tests/test_output.py
@@ -1,81 +1,198 @@
 import pytest
 import httpx
-from unittest.mock import patch, AsyncMock
+from unittest.mock import patch, AsyncMock, MagicMock
 from pytest_httpx import HTTPXMock, IteratorStream
 
-from app.pages.output import display_gemini_processed_markdown
+from app.pages.output import (
+    fetch_gemini_stream_data,
+    create_pdf_to_markdown_summary,
+)
 
 
 @pytest.mark.asyncio
-async def test_display_gemini_processed_markdown_success(httpx_mock: HTTPXMock) -> None:
+async def test_fetch_gemini_stream_data(httpx_mock: HTTPXMock) -> None:
+    BACKEND_DEV_API_URL = "http://ai-notebook-backend-1:8000/outputs/request_stream"
+
     filenames = ["test.pdf"]
     httpx_mock.add_response(
         method="POST",
-        url="http://127.0.0.1:8000/outputs/request",
+        url=BACKEND_DEV_API_URL,
         stream=IteratorStream([b"*Test Output*"]),
         status_code=200,
+        json=filenames,
     )
 
-    with patch("streamlit.empty", return_value=AsyncMock()) as mock_empty:
-        placeholder = mock_empty.return_value
-        placeholder.text = AsyncMock()
-        await display_gemini_processed_markdown(filenames)
-        placeholder.text.assert_called_once_with("*Test Output*")
+    async with httpx.AsyncClient() as client:
+        async with client.stream(
+            method="POST", url=BACKEND_DEV_API_URL, json=filenames
+        ) as response:
+            assert [part async for part in response.aiter_raw()] == [b"*Test Output*"]
 
 
 @pytest.mark.asyncio
-async def test_display_gemini_processed_markdown_unexpected_error(
-    httpx_mock: HTTPXMock,
-) -> None:
+async def test_fetch_gemini_stream_data_success(httpx_mock: HTTPXMock) -> None:
+    BACKEND_DEV_API_URL = "http://ai-notebook-backend-1:8000/outputs/request_stream"
     filenames = ["test.pdf"]
-    httpx_mock.add_exception(
+
+    httpx_mock.add_response(
         method="POST",
-        url="http://127.0.0.1:8000/outputs/request",
-        exception=Exception("予期せぬエラーが発生しました"),
+        url=BACKEND_DEV_API_URL,
+        stream=IteratorStream([b"chunk1", b"chunk2"]),
+        status_code=200,
+        headers={"accept": "text/event-stream"},
+        json=filenames,
     )
 
-    with (
-        patch("streamlit.empty", new_callable=AsyncMock) as mock_empty,
-        patch("streamlit.write", new_callable=AsyncMock) as mock_write,
-    ):
-        placeholder = mock_empty.return_value
-        await display_gemini_processed_markdown(filenames)
-        mock_write.assert_called_once_with(
-            "予期せぬエラーが発生しました。エラー： 予期せぬエラーが発生しました"
-        )
+    data = []
+    async for chunk in fetch_gemini_stream_data(filenames):
+        data.append(chunk)
+
+    assert data == ["chunk1", "chunk2"]
 
 
-# @pytest.mark.asyncio
-# async def test_display_gemini_processed_markdown_success(httpx_mock: HTTPXMock) -> None:
-#     httpx_mock.add_response(
-#         method="POST",
-#         url="https://localhost:8000/outputs/request",
-#         stream=IteratorStream([b"*Test Output*"]),
-#         status_code=200,
-#     )
+@pytest.mark.asyncio
+async def test_fetch_gemini_stream_data_http_error(httpx_mock: HTTPXMock) -> None:
+    BACKEND_DEV_API_URL = "http://ai-notebook-backend-1:8000/outputs/request_stream"
+    filenames = ["test.pdf"]
 
-#     with patch("streamlit.empty", return_value=AsyncMock()) as mock_empty:
-#         placeholder = mock_empty.return_value
-#         await display_gemini_processed_markdown()
-#         placeholder.write.assert_called_once_with("*Test Output*")
+    httpx_mock.add_exception(
+        exception=httpx.HTTPStatusError(
+            "エラーが発生しました",
+            request=httpx.Request(
+                "POST",
+                BACKEND_DEV_API_URL,
+                stream=IteratorStream([b"chunk1", b"chunk2"]),
+                json=filenames,
+                headers={"accept": "text/event-stream"},
+            ),
+            response=httpx.Response(400),
+        ),
+    )
+
+    async with httpx.AsyncClient() as client:
+        with pytest.raises(httpx.HTTPStatusError):
+            async with client.stream(
+                method="POST", url=BACKEND_DEV_API_URL, json=filenames
+            ):
+                pass
 
 
-# @pytest.mark.asyncio
-# async def test_display_gemini_processed_markdown_unexpected_error(
-#     httpx_mock: HTTPXMock,
-# ) -> None:
-#     httpx_mock.add_exception(
-#         method="POST",
-#         url="https://localhost:8000/outputs/request",
-#         exception=Exception("予期せぬエラーが発生しました"),
-#     )
+@pytest.mark.asyncio
+async def test_fetch_gemini_stream_data_remote_protocol_error(
+    httpx_mock: HTTPXMock,
+) -> None:
+    BACKEND_DEV_API_URL = "http://ai-notebook-backend-1:8000/outputs/request_stream"
+    filenames = ["test.pdf"]
 
-#     with (
-#         patch("streamlit.empty", new_callable=AsyncMock) as mock_empty,
-#         patch("streamlit.write", new_callable=AsyncMock) as mock_write,
-#     ):
-#         placeholder = mock_empty.return_value
-#         await display_gemini_processed_markdown()
-#         mock_write.assert_called_once_with(
-#             "予期せぬエラーが発生しました。エラー： 予期せぬエラーが発生しました"
-#         )
+    httpx_mock.add_exception(
+        exception=httpx.RemoteProtocolError(
+            "通信中にエラーが発生しました",
+            request=httpx.Request(
+                "POST",
+                BACKEND_DEV_API_URL,
+                stream=IteratorStream([b"chunk1", b"chunk2"]),
+                json=filenames,
+                headers={"accept": "text/event-stream"},
+            ),
+        ),
+    )
+
+    async with httpx.AsyncClient() as client:
+        with pytest.raises(httpx.RemoteProtocolError):
+            async with client.stream(
+                method="POST", url=BACKEND_DEV_API_URL, json=filenames
+            ):
+                pass
+
+
+@pytest.mark.asyncio
+async def test_fetch_gemini_stream_data_request_error(httpx_mock: HTTPXMock) -> None:
+    BACKEND_DEV_API_URL = "http://ai-notebook-backend-1:8000/outputs/request_stream"
+    filenames = ["test.pdf"]
+
+    httpx_mock.add_exception(
+        exception=httpx.RequestError(
+            "リクエストエラーが発生しました",
+            request=httpx.Request(
+                "POST",
+                BACKEND_DEV_API_URL,
+                stream=IteratorStream([b"chunk1", b"chunk2"]),
+                json=filenames,
+                headers={"accept": "text/event-stream"},
+            ),
+        ),
+    )
+
+    async with httpx.AsyncClient() as client:
+        with pytest.raises(httpx.RequestError):
+            async with client.stream(
+                method="POST", url=BACKEND_DEV_API_URL, json=filenames
+            ):
+                pass
+
+
+@pytest.mark.asyncio
+async def test_fetch_gemini_stream_data_timeout_error(httpx_mock: HTTPXMock) -> None:
+    BACKEND_DEV_API_URL = "http://ai-notebook-backend-1:8000/outputs/request_stream"
+    filenames = ["test.pdf"]
+
+    httpx_mock.add_exception(
+        exception=httpx.TimeoutException(
+            "タイムアウトしました",
+            request=httpx.Request(
+                "POST",
+                BACKEND_DEV_API_URL,
+                stream=IteratorStream([b"chunk1", b"chunk2"]),
+                json=filenames,
+                headers={"accept": "text/event-stream"},
+            ),
+        ),
+    )
+
+    async with httpx.AsyncClient() as client:
+        with pytest.raises(httpx.TimeoutException):
+            async with client.stream(
+                method="POST", url=BACKEND_DEV_API_URL, json=filenames
+            ):
+                pass
+
+
+@pytest.mark.asyncio
+async def test_fetch_gemini_stream_data_exception_error(httpx_mock: HTTPXMock) -> None:
+    BACKEND_DEV_API_URL = "http://ai-notebook-backend-1:8000/outputs/request_stream"
+    filenames = ["test.pdf"]
+
+    httpx_mock.add_exception(
+        exception=Exception(),
+    )
+
+    async with httpx.AsyncClient() as client:
+        with pytest.raises(Exception):
+            async with client.stream(
+                method="POST", url=BACKEND_DEV_API_URL, json=filenames
+            ):
+                pass
+
+
+@pytest.mark.asyncio
+async def test_create_pdf_to_markdown_summary(httpx_mock: HTTPXMock) -> None:
+    BACKEND_DEV_API_URL = "http://ai-notebook-backend-1:8000/outputs/request_stream"
+    filenames = ["test.pdf"]
+
+    httpx_mock.add_response(
+        method="POST",
+        url=BACKEND_DEV_API_URL,
+        stream=IteratorStream([b"chunk1"]),
+        status_code=200,
+        headers={"accept": "text/event-stream"},
+        json=filenames,
+    )
+
+    with patch("streamlit.empty") as empty_mock:
+        empty_mock.return_value = output = MagicMock()
+        output.markdown = MagicMock()
+
+        await create_pdf_to_markdown_summary(filenames)
+
+        empty_mock.assert_called_once()
+        output.markdown.assert_called_once_with("chunk1")
