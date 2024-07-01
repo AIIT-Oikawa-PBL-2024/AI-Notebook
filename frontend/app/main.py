05e8f8cd4061fa08a24b7cd41b3d62946043f109
Furumoto <a2329th@aiit.ac.jp>
2024-07-01 13:04:03 +0900

    chore: Update file upload functionality and sidebar design

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -1,14 +1,41 @@
 import os
-from typing import Any
-
+import logging
+import asyncio
 import httpx
 import streamlit as st
+from typing import Any
+from datetime import datetime
+from dotenv import load_dotenv
 
+# 環境変数を読み込む
+load_dotenv()
+
+# ログの設定
+logging.basicConfig(level=logging.INFO)
+
+# バックエンドAPIのURL
+BACKEND_HOST = os.getenv("BACKEND_HOST")
+BACKEND_DEV_API_URL = f"{BACKEND_HOST}/files/upload"
+
+# セッション状態の初期化
+if "note_name" not in st.session_state:
+    st.session_state.note_name = ""
+if "df" not in st.session_state:
+    st.session_state.df = None
+
+
+#サイドバーの設定
 with st.sidebar:
     st.page_link("main.py", label="トップページ")
     st.page_link("pages/study_ai_note.py", label="ノート・AIサポート学習帳")
     st.page_link("pages/select_files.py", label="ファイル選択")
+    
+# 時刻フォーマットを変換する関数
+def time_format(jst_str: str) -> str:
+    jst_time = datetime.fromisoformat(jst_str.replace("Z", "+00:00"))
+    return jst_time.strftime("%Y-%m-%d %H:%M:%S")
 
+#アップロード可能ファイルの拡張子を指定
 ALLOWED_EXTENSIONS = ["pdf", "jpg", "jpeg", "png"]
 
 
@@ -33,35 +60,43 @@ def is_valid_file(file: Any) -> bool:
     return True
 
 
-def main() -> None:
+async def main() -> None:
     """
     Main function for file upload.
 
     This function displays a file uploader and handles the file upload process.
     """
-    st.title("ファイルアップロード")
-    uploaded_files = st.file_uploader(
+st.title("ファイルアップロード")
+uploaded_files = st.file_uploader(
         "", accept_multiple_files=True, label_visibility="collapsed"
     )
 
-    if uploaded_files:
-        valid_files = []
-        file_names = set()
+# 学習帳名を入力
+st.text_input(
+"AI学習帳の名前を20文字以内で入力してください",
+key="note_input",
+value=st.session_state.note_name,
+max_chars=20,
+           )
 
-        for file in uploaded_files:
-            if file.name in file_names:
-                st.error(
-                    f" {file.name}ファイルは既にアップロードされています。"
-                    "他のファイルをアップロードしてください。"
+if uploaded_files:
+    valid_files = []
+    file_names = set()
+
+for file in uploaded_files:
+    if file.name in file_names:
+        st.error(
+            f" {file.name}ファイルは既にアップロードされています。"
+            "他のファイルをアップロードしてください。"
                 )
-                continue
+        continue
 
-            if is_valid_file(file):
-                valid_files.append(file)
-                file_names.add(file.name)
-                st.success(
-                    f" {file.name}は正常にアップロードされました。 (Size: "
-                    + f"{file.size / 1024 / 1024:.2f} MB)"
+    if is_valid_file(file):
+        valid_files.append(file)
+        file_names.add(file.name)
+        st.success(
+            f" {file.name}は正常にアップロードされました。 (Size: "
+            f"{file.size / 1024 / 1024:.2f} MB)"
                 )
 
         if valid_files:
@@ -70,7 +105,7 @@ def main() -> None:
                     with httpx.Client() as client:
                         files = {f.name: f.getvalue() for f in valid_files}
                         response = client.post(
-                            "http://127.0.0.1:8000/files/upload", files=files
+                            BACKEND_DEV_API_URL, files=files
                         )
 
                         if response.status_code == 200:
@@ -87,7 +122,7 @@ def main() -> None:
                     with httpx.Client() as client:
                         files = {f.name: f.getvalue() for f in valid_files}
                         response = client.post(
-                            "http://127.0.0.1:8000/files/upload", files=files
+                            BACKEND_DEV_API_URL, files=files
                         )
 
                         if response.status_code == 200:
@@ -99,7 +134,6 @@ def main() -> None:
                             )
                 except httpx.RequestError as e:
                     st.error(f"リクエストでエラーが発生しました：{e}")
-
 
 if __name__ == "__main__":
     main()
