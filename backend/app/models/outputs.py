23ab9a9d7aee97b56f421f9a8866699138955d6a
ysugimoto <ysugimoto@example.com>
2024-07-06 14:19:50 +0900

    add reStructuredText style docstring

--- backend/app/models/outputs.py
+++ backend/app/models/outputs.py
@@ -6,6 +6,18 @@ from app.database import Base
 
 # Ouputテーブルのモデルを定義
 class Output(Base):
+    """
+    出力データを表すクラスです。
+
+    Attributes:
+        id (int): 出力のID。
+        user_id (int): ユーザーのID。外部キー制約があります。
+        output (str): 出力データ。
+        created_at (datetime): 作成日時。
+        user (User): ユーザーとの関連。
+
+    """
+
     __tablename__ = "outputs"
 
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
