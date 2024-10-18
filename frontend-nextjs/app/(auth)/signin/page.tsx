13d2f9426eedd7d7751410fc79dd70cf3e98374a
sayzkkgw <sayzkkgw@gmail.com>
2024-10-18 10:29:35 +0900

    新しいフォルダ構成案に合わせて既存ファイル移動

---
+++ frontend-nextjs/app/(auth)/signin/page.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import { SignInForm } from "features/signin/SignInForm";
+
+const SignInPage: React.FC = () => {
+  return (
+    <main>
+      <SignInForm />
+    </main>
+  );
+};
+
+export default SignInPage;
--- frontend-nextjs/tailwind.config.ts
+++ frontend-nextjs/tailwind.config.ts
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
- 
+
 module.exports = withMT({
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./features/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
