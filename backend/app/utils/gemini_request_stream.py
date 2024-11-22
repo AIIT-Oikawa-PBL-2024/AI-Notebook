214947ac4f3d99b3f8da31977a400db3ba3880bc
ysugimoto <ysugimoto@example.com>
2024-11-22 19:27:57 +0900

    fixx ruff

--- backend/app/utils/gemini_request_stream.py
+++ backend/app/utils/gemini_request_stream.py
@@ -5,7 +5,6 @@ import os
 import unicodedata
 from typing import AsyncGenerator
 
-import ffmpeg
 import vertexai
 from dotenv import load_dotenv
 from google.api_core.exceptions import (
@@ -49,41 +48,6 @@ def check_file_exists(bucket_name: str, file_name: str) -> bool:
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(file_name)
     return blob.exists()
-
-
-# MP4のファイルをMP3に変換してGCSに保存
-def convert_mp4_to_mp3(bucket_name: str, file_name: str) -> bool:
-    # GCSからファイルを取得
-    client = storage.Client()
-    bucket = client.bucket(bucket_name)
-    blob = bucket.blob(file_name)
-
-    mp4_base_name = os.path.basename(file_name)
-    mp4_file_path = os.path.normpath(f"/tmp/{mp4_base_name}")
-    if not mp4_file_path.startswith("/tmp/"):
-        raise ValueError("Invalid file path")
-    blob.download_to_filename(mp4_file_path)
-
-    # MP4ファイルをMP3に変換
-    mp3_base_name = mp4_base_name.replace(".mp4", ".mp3")
-    mp3_file_path = os.path.normpath(f"/tmp/{mp3_base_name}")
-    if not mp3_file_path.startswith("/tmp/"):
-        raise ValueError("Invalid file path")
-    (
-        ffmpeg.input(mp4_file_path)
-        .output(mp3_file_path, format="mp3", acodec="libmp3lame")
-        .run()
-    )
-
-    # MP3ファイルをGCSにアップロード
-    upload_file_name = f'mp3/{file_name.replace(".mp4", ".mp3")}'
-    mp3_blob = bucket.blob(upload_file_name)
-    mp3_blob.upload_from_filename(mp3_file_path)
-
-    # 一時ファイルを削除
-    os.remove(mp4_file_path)
-    os.remove(mp3_file_path)
-    return mp3_blob.exists()
 
 
 # 複数のPDF, imageファイルを入力してコンテンツを生成
@@ -159,9 +123,7 @@ async def generate_content_stream(
                         mp3_files.append(mp3_file)
                     else:
                         logging.error(f"Failed to convert {file_name} to mp3 format.")
-                        raise InternalServerError(
-                            f"Failed to convert {file_name} to mp3 format."
-                        )
+                        raise InternalServerError(f"Failed to convert {file_name} to mp3 format.")
                 elif file_name.endswith(".mp3"):
                     # 音声ファイルのURL
                     mp3_file_url = f"gs://{bucket_name}/{file_name}"
